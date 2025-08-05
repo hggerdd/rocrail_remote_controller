86c369a2ba5a8d5748a46b877951ce5747c0c49f
hggg <heiko.greiner@googlemail.com>
2025-08-05 23:35:59 +0200

    ttt

--- firmware/lib/protocol/rocrail_protocol.py
+++ firmware/lib/protocol/rocrail_protocol.py
@@ -547,7 +547,7 @@ class RocrailProtocol:
         
         if self.socket_client:
             try:
-                message = f'<fn id="{current_loco_id}" fn0="{light_on_off}"/>'
+                message = f'<fn id="{current_loco_id}" fn=0 on="{light_on_off}"/>'
                 message_len = len(message)
                 message_and_header = f'<xmlh><xml size="{message_len}"/></xmlh>{message}'
                 self.socket_client.send(message_and_header.encode())
